e94c1b33494f767bf4b550ca734f98d2b596d789
iamvides <164425880+iamvides@users.noreply.github.com>
2025-06-08 21:32:11 -0500

    Update TestGrafo.java

--- SESION_9_AED/SESION_9_AED_EJER/src/test/TestGrafo.java
+++ SESION_9_AED/SESION_9_AED_EJER/src/test/TestGrafo.java
@@ -3,36 +3,21 @@ package test;
 import graph.GraphLink;
 
 public class TestGrafo {
-
     public static void main(String[] args) {
-
         GraphLink grafo = new GraphLink();
 
-        // Vértices y conexiones base
+        // Agregamos algunos vértices
         grafo.insertVertex("A");
         grafo.insertVertex("B");
         grafo.insertVertex("C");
         grafo.insertVertex("D");
+
+        // Conectamos los vértices
         grafo.insertEdge("A", "B");
         grafo.insertEdge("A", "C");
         grafo.insertEdge("B", "D");
-        grafo.insertEdge("C", "D");
 
-        System.out.println("Grafo original:");
+        // Mostramos el grafo
         grafo.mostrarGrafo();
-
-        // Prueba del EJERCICIO 1
-        grafo.removeEdge("A", "B");
-        System.out.println("\nDespués de eliminar arista A-B (EJERCICIO 1):");
-        grafo.mostrarGrafo();
-
-        // Prueba del EJERCICIO 2
-        grafo.removeVertex("C");
-        System.out.println("\nDespués de eliminar vértice C (EJERCICIO 2):");
-        grafo.mostrarGrafo();
-
-        // Prueba del EJERCICIO 3
-        System.out.println("\nRecorrido DFS desde A (EJERCICIO 3):");
-        grafo.dfs("A");
     }
 }
